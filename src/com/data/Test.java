575e496c9638cf7d22c36fbba8ef2073961da2ce
chennaishiva <chennaisiva21@gmail.com>
2023-01-03 13:28:05 +0530

    A works

---
+++ src/com/data/Test.java
@@ -0,0 +1,16 @@
+package com.data;
+
+public class Test {
+	
+	
+	private void add() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.add();
+	}
+
+}
